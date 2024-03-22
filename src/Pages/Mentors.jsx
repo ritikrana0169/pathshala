13de90361549c0212496b56fe3cb7ce791b7d372
unknown <saviji520@gmail.com>
2024-03-22 23:47:24 +0530

    mentors on progress

--- src/Pages/Mentors.jsx
+++ src/Pages/Mentors.jsx
@@ -1,7 +1,58 @@
 import React from "react";
-
+import "../Styles/Mentors.css";
+import aboutschool from "../assets/aboutschool1.jpg";
+import principal from "../assets/principal.jpg";
+import mission from "../assets/mission.jpg";
 const Mentors = () => {
-  return <div>Mentors</div>;
+  return (
+    <div>
+
+      <div className="learning">
+
+        <div>
+          <div>
+            <h1>About <br />Our School</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, aspernatur, architecto minus fuga repellat quisquam velit beatae maiores ullam autem eos sint quas cum suscipit veniam deleniti dignissimos esse exercitationem placeat ipsa reprehenderit sequi? Laudantium fugiat nemo aspernatur molestias ut, eligendi dolore ab illum ipsam nulla quis voluptas perspiciatis voluptates at facilis dicta incidunt eos alias saepe iure. Quo similique asperiores obcaecati nulla at cumque iure </p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <img src={aboutschool} alt="err" />
+          </div>
+        </div>
+
+      </div>
+
+      <div className="welcome">
+      <div className="hardColor">
+          <div>
+            <img src={principal} alt="err" />
+            <h1>Principal’s Welcome</h1>
+            <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.
+              This is a great space to write a long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.</p>
+          </div>
+        </div>
+        <div className="softColor">
+          <div>
+            <img src={mission} alt="err" />
+            <h1>Mission</h1>
+            <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.
+              This is a great space to write a long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.</p>
+          </div>
+        </div>
+
+
+
+
+
+      </div>
+
+
+
+
+    </div>
+  );
 };
 
 export default Mentors;

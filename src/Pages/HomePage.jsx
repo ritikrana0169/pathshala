782af3b28a00a308a8c62fb8bb3be13b75a3cd6b
Sachin <vsachin7269@gmail.com>
2024-03-23 22:12:37 +0530

    merging

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import "../Styles/HomePage.css";
 import studentPic from "../assets/students.png";
 import safetyPic from "../assets/safety.png";
+import approachImg3 from "../assets/approach3.png";
+import approachImg4 from "../assets/approach4.png";
 
 const HomePage = () => {
   return (
@@ -33,6 +35,59 @@ const HomePage = () => {
               in-person learning model
             </p>
             <button className="aboutUsBtn">Learn More</button>
+          </div>
+        </div>
+      </div>
+      <div className="approach">
+        <div className="approachHeading">
+          <h1>Why Stanford Elementary</h1>
+        </div>
+        <div className="approachDetails">
+          <div className="approachImg1 backgroundImg">
+            <h3>OUR APPROACH</h3>
+            <p>
+              I'm a paragraph. Click here to add your own text and edit me. Let
+              your users get to know you.
+            </p>
+            <button className="aboutUsBtn btnOnImg">Learn More</button>
+          </div>
+          <div className="admission">
+            <h3>ADDMISSION</h3>
+            <p>
+              I'm a paragraph. Click here to add your own text and edit me. Let
+              your users get to know you.
+            </p>
+            <button className="aboutUsBtn">Learn More</button>
+          </div>
+          <div className="approachImg2 backgroundImg">
+            <h3>Enhance Learning</h3>
+            <p>
+              I'm a paragraph. Click here to add your own text and edit me. Let
+              your users get to know you.
+            </p>
+            <button className="aboutUsBtn btnOnImg">Learn Programs</button>
+          </div>
+          <div className="admission">
+            <h3>Latest News & Events</h3>
+            <p>
+              I'm a paragraph. Click here to add your own text and edit me. Let
+              your users get to know you.
+            </p>
+            <button className="aboutUsBtn">View All</button>
+          </div>
+          <div>
+            <img src={approachImg3} alt="" />
+          </div>
+          <div className="admission">
+            <h3>For the Parents</h3>
+            <p>
+              I'm a paragraph. Click here to add your own text and edit me. Let
+              your users get to know you.
+            </p>
+            <button className="aboutUsBtn">Get Informed</button>
+          </div>
+          <div>
+            <img src={approachImg4} alt="" />
           </div>
         </div>
       </div>
